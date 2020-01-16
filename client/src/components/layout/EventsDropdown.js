1b03ad28a8dab380a6a25565ded80285e465e1e9
b3ling <b3ling@uwaterloo.ca>
2020-01-16 00:22:58 -0500

    Added test event only visibile to admin

--- client/src/components/layout/EventsDropdown.js
+++ client/src/components/layout/EventsDropdown.js
@@ -18,15 +18,26 @@ class EventsDropdown extends Component{
         this.setState({event: this.props.currentEvent})
     }
     render(){
+    console.log(this.props)
     return(
     <div className="events-dropdown" style={{position:"relative"}}>
         <img style={{position:"absolute", top:"10px", left:10}} width="40px;" alt={this.state.event}src={"/images/"+this.state.event+".png"}/>
         <select value={this.state.event?(this.state.event):("")} id='event' style={{paddingLeft:50}} name="team" required onChange={this.handleChange}>
             {
                 this.props.eventData && Object.entries(this.props.eventData).map(event=>{
-                    return(
+                    if(event[1].displayName === "olaetest"){
+                        if(this.props.auth === "Fn322uSNGRd6bWPu3hYFtFzVTgQ2"){
+                            return(
                         <option key={event[0]} value={event[0]}>{event[1] && event[1].displayName}</option>
+                        );
+                        }else{return null}
+                    
+                    }else{
+                    return(
+                    <option key={event[0]} value={event[0]}>{event[1] && event[1].displayName}</option>
                     );
+                    }
+                    
                 })
             }
         </select>
@@ -38,6 +49,7 @@ class EventsDropdown extends Component{
 const mapStateToProps = (state, props) =>{
     return{
         eventData: state.firebase.data.eventData,
+        auth: state.firebase.auth.uid
     }
 }
 
